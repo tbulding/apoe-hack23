764652b941636798737978b7b1704a32d60d1c76
Tony Bulding <tbulding@amazon.com>
2023-09-19 19:45:24 -0500

    adding test scaffolding

---
+++ .project_automation/static_tests/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+
+## NOTE: paths may differ when running in a managed task. To ensure behavior is consistent between
+# managed and local tasks always use these variables for the project and project type path
+PROJECT_PATH=${BASE_PATH}/project
+PROJECT_TYPE_PATH=${BASE_PATH}/projecttype
+
+ruff check $PROJECT_PATH/src/*
+semgrep scan --config auto $PROJECT_PATH/src/*
